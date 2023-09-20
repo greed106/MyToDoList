d5740ff03000a96075184fbeb8ff677c7abdb7d9
明佳 杨 <ymj1062415694@outlook.com>
2023-09-20 09:06:18 +0800

    初始化提交

---
+++ src/main/java/com/ymj/mytodolist/MyToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.ymj.mytodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyToDoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyToDoListApplication.class, args);
+    }
+
+}
